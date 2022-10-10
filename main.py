1e16a383fc044a851684ac810e325c5809f120bf
Marko <marko@MacBook-Pro-Marko.local>
2022-10-10 15:54:55 +0300

    BE #1 task done

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"status": "Working"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
